4998d27e08f7f0f56624dba4b9504cc510e5548f
OropesaJayme <oropesajayme@gmail.com>
2018-10-25 22:48:41 +0800

    Exercise 1

---
+++ oropesa_e1.py
@@ -0,0 +1,12 @@
+data = {"Name" :"Jayme Angeli Oropesa",
+"Spirit Animal": "Tiger",
+"Reason": "I am brave enough to face all my challenges in my life",
+"Hobby" : "Reading Books",
+"Profession" : "Civil Engineer"
+}
+
+print ( " I am {}".format(data["Name"]))
+print ("My Spirit Animal is {}".format(data["Spirit Animal"]))
+print ("Because {}".format(data["Reason"]))
+print ("When not to school i love to {}".format (data["Hobby"]))
+print ("I dream of Being a {}".format(data["Profession"]))
